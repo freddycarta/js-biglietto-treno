7e19a26026e71da8c8ce36b482da6a8c5533035e
freddycarta <cartafederico92@gmail.com>
2022-07-20 16:21:03 +0200

    js 2

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+
+
 console.log ('js ok');
 
-const km = (prompt ('quanti km vuoi percorrere?', '15'))
+const km = parseInt( prompt('Quanti km vuoi percorrere?', '10') )
+console.log(km);
 
-const age = (prompt ('quanti anni hai?', '29'))
+const age = parseInt ( prompt("quanti hanni hai?" , '29') )
+console.log(age);
 
+let ticket = (km * 0.21);
+console.log(ticket);
+
+if(age > 65) {
+    ticket = (ticket - (ticket * 0.40))
+} else if (age < 18 ) {
+    ticket = (ticket - (ticket * 0.20));
+}
+
+const finalTicket = document .getElementById('finalTicket')
+
+document.getElementById('finalTicket').innerText = 'Il tuo biglietto costa'+' '+ ticket +' '+ '€'
